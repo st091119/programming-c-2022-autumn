e7c96cf37badf31411135d52a6e86f544b1983a1
st091119 <reutskaya1234@gmail.com>
2022-12-12 14:17:32 +0300

    Work

--- 2022.12.12-Very-very-hard-work/Task1/Source.cpp
+++ 2022.12.12-Very-very-hard-work/Task1/Source.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+int m(int** desk, int m, int n)
+{
+	desk = { 0 };
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < m; ++i)
+		{
+			if()
+		}
+	}
 
+}
 int main(int argc, char* argv[])
 {
 
