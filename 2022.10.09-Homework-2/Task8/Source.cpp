58df90bc49787eac37741bd424f2098f496d71c1
st091119 <reutskaya1234@gmail.com>
2022-12-15 14:13:58 +0300

    Fix

--- 2022.10.09-Homework-2/Task8/Source.cpp
+++ 2022.10.09-Homework-2/Task8/Source.cpp
@@ -1,81 +1,28 @@
-#include <iostream>
+#include<iostream>
 
-int main(int argc, char* argv[])
+int main(int arg, char* argv[])
 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
-	int m1 = 0;
-	int m2 = 0;
-	int m3 = 0;
+
 	std::cin >> a >> b >> c;
 
-	if (a > b && a > c)
+	if ((a + b <= c) || (b + c <= a) || (a + c <= b))
 	{
-		m1 = a;
-		m2 = b;
-		m3 = c;
+		std::cout << "impossible";
 	}
-	else if (b > a && b > c)
+	else if ((a * a + b * b == c * c) || (a * a > b * b == c * c) || (b * b > a * a == c * c))
 	{
-		m1 = b;
-		m2 = a;
-		m3 = c;
+		std::cout << "right";
 	}
-	else if (c > a && c > b)
+	else if ((a * a + b * b > c * c) && (a * a < b * b + c * c) && (b * b < a * a + c * c))
 	{
-		m1 = c;
-		m2 = a;
-		m3 = b;
-	}
-	else if (c = b = a)
-	{
-		m1 = m2 = m3 = a;
-	}
-	else if (a > b && b == c)
-	{
-		m1 = a;
-		m2 = m3 = b;
-	}
-	else if (a == b && a > c)
-	{
-		m1 = m2 = a;
-		m3 = c;
-	}
-	else if (b > a && a == c)
-	{
-		m1 = b;
-		m2 = m3 = a;
-	}
-	else if (b == c && a < b)
-	{
-		m1 = m2 = b;
-		m3 = a;
-	}
-	else if (c > b && a == b)
-	{
-		m1 = c;
-		m2 = m3 = a;
-	}
-
-	if (a + b > c || a + c > b || b + c > a)
-	{
-		if (m2 * m2 + m3 * m3 > m1 * m1)
-		{
-			std::cout << "acute";
-		}
-		else if (m2 * m2 + m3 * m3 < m1 * m1)
-		{
-			std::cout << "obtuse";
-		}
-		else if (m2 * m2 + m3 * m3 == m1 * m1)
-		{
-			std::cout << "right";
-		}
+		std::cout << "acute";
 	}
 	else
 	{
-		std::cout << "impossible";
+		std::cout << "obtuse";
 	}
 
 	return EXIT_SUCCESS;
